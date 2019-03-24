b4ab171120d191fafb5aad465642cc39310bfee0
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2019-03-24 07:58:17 -0300

    Commit

--- src/app/audit/audit.page.ts
+++ src/app/audit/audit.page.ts
@@ -7,12 +7,33 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./audit.page.scss'],
 })
 export class AuditPage implements OnInit {
+  tab: string = 'pending';
+  pendingList = [];
+  completedList = [];
 
   constructor(
     public navCtrl: NavController
   ) { }
 
   ngOnInit() {
+  }
+
+  onTabClick(tab) {
+    this.tab = tab;
+
+    this.pendingList = [
+      {
+        id: 1,
+        title: 'Auditoria 24/03/2019'
+      }
+    ]
+
+    this.completedList = [
+      {
+        id: 1,
+        title: 'Auditoria 20/03/2019'
+      }
+    ]
   }
 
 }
